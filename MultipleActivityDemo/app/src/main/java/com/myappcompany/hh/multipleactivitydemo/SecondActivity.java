6a2c8943de5211ecd77be8a19f2c2ab420b6b501
Hetul <hetulchaudhary786@gmail.com>
2022-07-30 12:54:48 +0530

    change in memorablePlaces and sharedPreferences

---
+++ MultipleActivityDemo/app/src/main/java/com/myappcompany/hh/multipleactivitydemo/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.myappcompany.hh.multipleactivitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+    public void goToPrevious(View view){
+//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(intent);
+        finish();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String name= intent.getStringExtra("name");
+
+
+//        int age = intent.getIntExtra("age", 0);
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+
+
+    }
+}
